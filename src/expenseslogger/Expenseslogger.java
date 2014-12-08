f0176155e751347a953a1371dc77c35dc6024fe4
Affan Hasan <affan.hasan88@gmail.com>
2014-12-08 11:44:13 +0500

    Pushing to remote

--- src/expenseslogger/Expenseslogger.java
+++ src/expenseslogger/Expenseslogger.java
@@ -14,7 +14,7 @@ public class Expenseslogger {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         
     }
 }
